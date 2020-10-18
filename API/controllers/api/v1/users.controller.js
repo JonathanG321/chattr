02f6760e4ba3c2196851511a62d31e22ebaac5cf
JonathanG321 <JonathanG321@ymail.com>
2020-10-17 21:44:11 -0400

    add users controller

---
+++ API/controllers/api/v1/users.controller.js
@@ -0,0 +1,55 @@
+const User = require('../../../models/user.model');
+const { RecordNotFoundError } = require('../../api.controller');
+
+module.exports = {
+  async create(request, response, next) {
+    try {
+      const { username, password = '', passwordConfirmation, email = '' } = request.body;
+      const newUser = {
+        username,
+        avatar,
+        password,
+        passwordConfirmation,
+        email: email.toLowerCase(),
+      };
+      const user = await User.create(newUser);
+      request.session.userId = user.id;
+      response.json({ id: user.id });
+    } catch (e) {
+      next(e);
+    }
+  },
+  async update(request, response, next) {
+    try {
+      const { id } = request.params;
+      const { username, avatar } = request.body;
+      const newUser = {
+        username,
+        avatar,
+      };
+      const user = await User.update(newUser, { where: id });
+      response.json({ user });
+    } catch (e) {
+      next(e);
+    }
+  },
+  async show(request, response, next) {
+    try {
+      const { id } = request.params;
+      const user = await User.findOne({ where: { id } });
+      if (!user) {
+        throw new RecordNotFoundError(User, id);
+      }
+      response.json({ user });
+    } catch (e) {
+      next(e);
+    }
+  },
+  async currentUser(request, response, next) {
+    try {
+      response.json(response.locals.currentUser);
+    } catch (e) {
+      next(e);
+    }
+  },
+};
