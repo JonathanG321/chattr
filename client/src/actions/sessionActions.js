23e713f37bbade1f7727ceb9f27c7ddefce468cc
JonathanG321 <JonathanG321@ymail.com>
2020-10-18 23:38:48 -0400

    add arguments to create session and create user

--- client/src/actions/sessionActions.js
+++ client/src/actions/sessionActions.js
@@ -3,7 +3,7 @@ import { SIGN_IN, SIGN_UP, SIGN_OUT } from './types';
 import { Session } from '../requests/session';
 import { User } from '../requests/user';
 
-const createSession = () => (dispatch) => {
+const createSession = (userCredentials) => (dispatch) => {
   Session.create(userCredentials).then((user) => {
     dispatch({ type: SIGN_IN, payload: user });
   });
@@ -13,8 +13,8 @@ const destroySession = () => (dispatch) => {
     dispatch({ type: SIGN_OUT });
   });
 };
-const createUser = () => (dispatch) => {
-  User.create().then(() => {
+const createUser = (newUser) => (dispatch) => {
+  User.create(newUser).then((user) => {
     dispatch({ type: SIGN_UP, payload: user });
   });
 };
