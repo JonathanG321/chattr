0e3a4161f28c96f3c7b3af35e74d0b53b9737c49
JonathanG321 <JonathanG321@ymail.com>
2020-10-19 00:24:01 -0400

    create SignInPage

---
+++ client/src/components/SignInPage.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withRouterPropTypes } from '../PropTypes/withRouterPropTypes';
+import SessionHOC from '../HigherOrderComponents/SessionHOC';
+
+function SignInPage(props) {
+  const { onSignIn } = props;
+  return (
+    <div className="ml-3 mr-3 mt-2">
+      <h1>Sign In</h1>
+      <form onSubmit={onSignIn}>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" />
+        </div>
+        <input type="submit" value="Login" />
+      </form>
+    </div>
+  );
+}
+
+SignInPage.propTypes = {
+  onSignIn: PropTypes.func.isRequired,
+};
+
+export default SessionHOC(SignInPage);
