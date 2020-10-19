169e750a0eab909aa8193c8dc510b8205e95bcbe
JonathanG321 <JonathanG321@ymail.com>
2020-10-19 09:25:22 -0400

    refactor SessionHOC

--- client/src/HigherOrderComponents/SessionHOC.js
+++ client/src/HigherOrderComponents/SessionHOC.js
@@ -15,25 +15,27 @@ function SessionHOC(Component) {
       mapStateToProps,
       mapDispatchToProps,
     )((props) => {
-      async function signIn(event) {
+      function signIn(event) {
         event.preventDefault();
         const formData = new FormData(event.currentTarget);
         const credentials = {
           email: formData.get('email'),
           password: formData.get('password'),
         };
-        const { user, errors } = await props.createSession(credentials);
-        if (user.id && !errors) {
-          props.history.push('/');
-        } else {
-          alert('Login Failed');
-        }
+        props.createSession(credentials).then((user, errors) => {
+          if (user.id && !errors) {
+            props.history.push('/');
+          } else {
+            console.log(user);
+            alert('Login Failed');
+          }
+        });
       }
-      async function signOut() {
-        await props.destroySession();
+      function signOut() {
+        props.destroySession();
         props.history.push('/');
       }
-      async function signUp(event) {
+      function signUp(event) {
         event.preventDefault();
         const formData = new FormData(event.currentTarget);
         const newUser = {
@@ -43,12 +45,13 @@ function SessionHOC(Component) {
           password: formData.get('password'),
           passwordConfirmation: formData.get('password-confirmation'),
         };
-        const { user, errors } = await props.createUser(newUser);
-        if (user.id && !errors) {
-          props.history.push('/');
-        } else {
-          alert('Sign Up Failed');
-        }
+        props.createUser(newUser).then((user, errors) => {
+          if (user.id && !errors) {
+            props.history.push('/');
+          } else {
+            alert('Sign Up Failed');
+          }
+        });
       }
       return <Component {...props} onSignIn={signIn} onSignUp={signUp} onSignOut={signOut} />;
     }),
