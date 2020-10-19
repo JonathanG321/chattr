9e517434d7b68d3872691f43abd1157a856466d8
JonathanG321 <JonathanG321@ymail.com>
2020-10-19 09:25:03 -0400

    refactor sessionActions

--- client/src/actions/sessionActions.js
+++ client/src/actions/sessionActions.js
@@ -3,20 +3,15 @@ import { SIGN_IN, SIGN_UP, SIGN_OUT } from './types';
 import { Session } from '../requests/session';
 import { User } from '../requests/user';
 
-const createSession = (userCredentials) => (dispatch) => {
-  Session.create(userCredentials).then((user) => {
-    dispatch({ type: SIGN_IN, payload: user });
-  });
-};
-const destroySession = () => (dispatch) => {
-  Session.destroy().then(() => {
-    dispatch({ type: SIGN_OUT });
-  });
-};
-const createUser = (newUser) => (dispatch) => {
-  User.create(newUser).then((user) => {
-    dispatch({ type: SIGN_UP, payload: user });
-  });
-};
+const createSession = (userCredentials) => (dispatch) =>
+  Session.create(userCredentials).then(
+    (user) => dispatch({ type: SIGN_IN, payload: user }).payload,
+  );
+
+const destroySession = () => (dispatch) =>
+  Session.destroy().then(() => dispatch({ type: SIGN_OUT }));
+
+const createUser = (newUser) => (dispatch) =>
+  User.create(newUser).then((user) => dispatch({ type: SIGN_UP, payload: user }).payload);
 
 export { createSession, destroySession, createUser };
