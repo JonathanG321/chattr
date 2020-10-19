639c1ec2e21737ae5e00bd50db19b690448e7c7f
JonathanG321 <JonathanG321@ymail.com>
2020-10-19 00:23:49 -0400

    add more to signIn and signUp functions in SessionHOC

--- client/src/HigherOrderComponents/SessionHOC.js
+++ client/src/HigherOrderComponents/SessionHOC.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import { createSession, destroySession, createUser } from '../actions/sessionActions';
 
 function SessionHOC(Component) {
@@ -8,21 +9,51 @@ function SessionHOC(Component) {
     destroySession,
     createUser,
   };
-  return connect(
-    mapStateToProps,
-    mapDispatchToProps,
-  )((props) => {
-    function signIn(credentials) {
-      props.createSession(credentials);
-    }
-    function signOut() {
-      props.destroySession();
-    }
-    function signUp(newUser) {
-      props.createUser(newUser);
-    }
-    return <Component {...props} onSignIn={signIn} onSignUp={signUp} onSignOut={signOut} />;
-  });
+  return withRouter(
+    connect(
+      mapStateToProps,
+      mapDispatchToProps,
+    )((props) => {
+      function signIn(event) {
+        event.preventDefault();
+        const formData = new FormData(event.currentTarget);
+        const credentials = {
+          email: formData.get('email'),
+          password: formData.get('password'),
+        };
+        props.createSession(credentials).then(({ user, errors }) => {
+          if (user.id && !errors) {
+            props.history.push('/');
+          } else {
+            alert('Login Failed');
+          }
+        });
+      }
+      function signOut() {
+        props.destroySession();
+        props.history.push('/');
+      }
+      function signUp(event) {
+        event.preventDefault();
+        const formData = new FormData(event.currentTarget);
+        const newUser = {
+          username: formData.get('username'),
+          email: formData.get('email'),
+          avatar: formData.get('avatar'),
+          password: formData.get('password'),
+          passwordConfirmation: formData.get('password-confirmation'),
+        };
+        props.createUser(newUser).then(({ user, errors }) => {
+          if (user.id && !errors) {
+            props.history.push('/');
+          } else {
+            alert('Sign Up Failed');
+          }
+        });
+      }
+      return <Component {...props} onSignIn={signIn} onSignUp={signUp} onSignOut={signOut} />;
+    }),
+  );
 }
 
 export default SessionHOC;
