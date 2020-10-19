dcc2b839b6339a9dc15e285738b746017aeb8e49
JonathanG321 <JonathanG321@ymail.com>
2020-10-19 00:08:45 -0400

    create SessionHOC

---
+++ client/src/HigherOrderComponents/SessionHOC.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { createSession, destroySession, createUser } from '../actions/sessionActions';
+
+function SessionHOC(Component) {
+  const mapStateToProps = (state) => ({});
+  const mapDispatchToProps = {
+    createSession,
+    destroySession,
+    createUser,
+  };
+  return connect(
+    mapStateToProps,
+    mapDispatchToProps,
+  )((props) => {
+    function signIn(credentials) {
+      props.createSession(credentials);
+    }
+    function signOut() {
+      props.destroySession();
+    }
+    function signUp(newUser) {
+      props.createUser(newUser);
+    }
+    return <Component {...props} onSignIn={signIn} onSignUp={signUp} onSignOut={signOut} />;
+  });
+}
+
+export default SessionHOC;
