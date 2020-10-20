f6146f85cebb60ad6463b9df78a37dd27d979ed0
JonathanG321 <JonathanG321@ymail.com>
2020-10-19 20:54:07 -0400

    add error handling for createSession and createUser actions

--- client/src/actions/sessionActions.js
+++ client/src/actions/sessionActions.js
@@ -1,17 +1,18 @@
-import io from 'socket.io-client';
-import { SIGN_IN, SIGN_UP, SIGN_OUT } from './types';
+import { SIGN_IN, SIGN_UP, SIGN_OUT, SIGN_IN_FAILURE, SIGN_UP_FAILURE } from './types';
 import { Session } from '../requests/session';
 import { User } from '../requests/user';
 
 const createSession = (userCredentials) => (dispatch) =>
-  Session.create(userCredentials).then(
-    (user) => dispatch({ type: SIGN_IN, payload: user }).payload,
-  );
+  Session.create(userCredentials)
+    .then((user) => dispatch({ type: SIGN_IN, payload: user }).payload)
+    .catch((error) => dispatch({ type: SIGN_IN_FAILURE, payload: error.serverErrors.errors }));
 
 const destroySession = () => (dispatch) =>
   Session.destroy().then(() => dispatch({ type: SIGN_OUT }));
 
 const createUser = (newUser) => (dispatch) =>
-  User.create(newUser).then((user) => dispatch({ type: SIGN_UP, payload: user }).payload);
+  User.create(newUser)
+    .then((user) => dispatch({ type: SIGN_UP, payload: user }).payload)
+    .catch((error) => dispatch({ type: SIGN_UP_FAILURE, payload: error.serverErrors.errors }));
 
 export { createSession, destroySession, createUser };
