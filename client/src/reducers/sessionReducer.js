33163cd818c7a7bf62de3d475b5bd735a7c3f913
JonathanG321 <JonathanG321@ymail.com>
2020-10-19 20:58:19 -0400

    add and remove errors from state in sessionReducer

--- client/src/reducers/sessionReducer.js
+++ client/src/reducers/sessionReducer.js
@@ -1,7 +1,8 @@
-import { SIGN_IN, SIGN_UP, SIGN_OUT } from '../actions/types';
+import { SIGN_IN, SIGN_UP, SIGN_OUT, SIGN_IN_FAILURE, SIGN_UP_FAILURE } from '../actions/types';
 
 const initialState = {
   user: null,
+  errors: null,
 };
 
 export default function (state = initialState, action) {
@@ -10,16 +11,30 @@ export default function (state = initialState, action) {
       return {
         ...state,
         user: action.payload,
+        errors: null,
       };
     case SIGN_UP:
       return {
         ...state,
         user: action.payload,
+        errors: null,
       };
     case SIGN_OUT:
       return {
         ...state,
         user: null,
+      };
+    case SIGN_IN_FAILURE:
+      return {
+        ...state,
+        user: null,
+        errors: action.payload,
+      };
+    case SIGN_UP_FAILURE:
+      return {
+        ...state,
+        user: null,
+        errors: action.payload,
       };
     default:
       return state;
