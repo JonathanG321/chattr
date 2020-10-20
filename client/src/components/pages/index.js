f03bee285daed99817bb368063156e5bdac0f6b7
JonathanG321 <JonathanG321@ymail.com>
2020-10-20 12:45:51 -0400

    export Homepage

--- client/src/components/pages/index.js
+++ client/src/components/pages/index.js
@@ -1,5 +1,6 @@
 import NotFoundPage from './NotFoundPage';
 import SignInPage from './SignInPage';
 import SignUpPage from './SignUpPage';
+import HomePage from './HomePage';
 
-export { SignInPage, SignUpPage, NotFoundPage };
+export { SignInPage, SignUpPage, NotFoundPage, HomePage };
