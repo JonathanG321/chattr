fb640e6b276ac2f0c4255091181d882b25b1ab29
JonathanG321 <JonathanG321@ymail.com>
2020-10-20 14:13:19 -0400

    change HomePage to class component and sign out any user on mount

--- client/src/components/pages/HomePage/index.js
+++ client/src/components/pages/HomePage/index.js
@@ -1,20 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import SessionHOC from '../../../HigherOrderComponents/SessionHOC';
 
-function HomePage(props) {
-  return (
-    <div className="home-page">
-      <div className="auth-card justify-center-vertical align-items-center-vertical">
-        <h1 className="title">Chattr</h1>
-        <Link className="max-width" to="sign-in">
-          <button className="button max-width">Sign in with Existing Account</button>
-        </Link>
-        <Link className="max-width" to="/sign-up">
-          <button className="button mt-1 max-width">Sign Up with New Account</button>
-        </Link>
+class HomePage extends Component {
+  componentDidMount() {
+    const { onSignOut } = this.props;
+    onSignOut();
+  }
+  render() {
+    return (
+      <div className="home-page">
+        <div className="auth-card justify-center-vertical align-items-center-vertical">
+          <h1 className="title">Chattr</h1>
+          <Link className="max-width" to="sign-in">
+            <button className="button max-width">Sign in with Existing Account</button>
+          </Link>
+          <Link className="max-width" to="/sign-up">
+            <button className="button mt-1 max-width">Sign Up with New Account</button>
+          </Link>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
-export default HomePage;
+export default SessionHOC(HomePage);
