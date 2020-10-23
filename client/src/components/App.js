4bfef8f98d4fc8a62a4495fbc3edcb1429f9c277
JonathanG321 <JonathanG321@ymail.com>
2020-10-23 15:38:58 -0400

    add isLoading to App

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,12 +2,20 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { NotFoundPage, SignInPage, SignUpPage, HomePage, ChatDisplay } from './pages';
 import AuthRoute from './common/AuthRoute';
+import Loading from './common/Loading';
 import SessionHOC from '../HigherOrderComponents/SessionHOC';
 import './App.scss';
 
 class App extends Component {
+  componentDidMount() {
+    const { getCurrentUser } = this.props;
+    getCurrentUser();
+  }
   render() {
-    const { user } = this.props;
+    const { user, isLoading } = this.props;
+    if (isLoading) {
+      return <Loading />;
+    }
     return (
       <Router>
         <Switch>
@@ -20,7 +28,7 @@ class App extends Component {
           <Route exact path="/sign-up">
             <SignUpPage />
           </Route>
-          <AuthRoute isSignedIn={!!user} path="/chat">
+          <AuthRoute isSignedIn={!!user && !!user.id} path="/chat">
             <ChatDisplay user={user} />
           </AuthRoute>
           <Route path="*">
