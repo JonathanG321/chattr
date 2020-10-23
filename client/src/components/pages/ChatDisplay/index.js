61cbfc54f90dbf8108a3b35e1e032b12c850ca0c
JonathanG321 <JonathanG321@ymail.com>
2020-10-23 18:04:50 -0400

    add MessageForm to ChatDisplay

--- client/src/components/pages/ChatDisplay/index.js
+++ client/src/components/pages/ChatDisplay/index.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import UserAvatar from '../../common/UserAvatar';
+import MessageForm from '../../common/MessageForm';
+import RoomHOC from '../../../HigherOrderComponents/RoomHOC';
 import '../styles.scss';
 
 export class ChatDisplay extends Component {
   render() {
-    const { user } = this.props;
+    const { user, addMessage, sendMessage, addRoom, currentRoom } = this.props;
+    // sendMessage('hello');
     return (
       <main className="max-height">
         <nav className="navbar">
@@ -14,15 +17,20 @@ export class ChatDisplay extends Component {
             </h2>
             <UserAvatar user={user} />
           </div>
-          <div className="room-display"></div>
+          <div className="room-display">{currentRoom}</div>
         </nav>
         <div className="flex max-height-no-nav">
           <div className="rooms-nav"></div>
-          <div className="chat"></div>
+          <div className="chat">
+            <div className="message-box"></div>
+            <div className="chat-form">
+              <MessageForm onSubmit={sendMessage} username={user.username} />
+            </div>
+          </div>
         </div>
       </main>
     );
   }
 }
 
-export default ChatDisplay;
+export default RoomHOC(ChatDisplay);
