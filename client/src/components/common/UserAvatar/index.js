e49d5e6c8a901ab99857c72a01ca32b5c9e60c54
JonathanG321 <JonathanG321@ymail.com>
2020-10-23 15:37:26 -0400

    add alt prop to UserAvatar Component

--- client/src/components/common/UserAvatar/index.js
+++ client/src/components/common/UserAvatar/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function UserAvatar(props) {
   const { user } = props;
-  return <img className="user-avatar" src={`${user.avatar}`} />;
+  return <img className="user-avatar" src={`${user.avatar}`} alt={user.username} />;
 }
 
 export default UserAvatar;
