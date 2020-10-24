91bbbdc51ae937c905e3479992b02641987da69f
JonathanG321 <JonathanG321@ymail.com>
2020-10-24 17:53:46 -0400

    remove addMessage and add changeRooms action

--- client/src/actions/roomActions.js
+++ client/src/actions/roomActions.js
@@ -1,11 +1,11 @@
-import { ADD_ROOM, ADD_MESSAGE } from './types';
+import { ADD_ROOM, ADD_MESSAGE, CHANGE_ROOM } from './types';
 
 const addRoom = (room) => async (dispatch) => {
   dispatch({ type: ADD_ROOM, payload: room });
 };
 
-const addMessage = (data) => async (dispatch) => {
-  dispatch({ type: ADD_MESSAGE, payload: data });
+const changeRoom = (roomName) => async (dispatch) => {
+  dispatch({ type: CHANGE_ROOM, payload: roomName });
 };
 
 const sendMessage = (data) => async (dispatch, ...args) => {
@@ -13,4 +13,4 @@ const sendMessage = (data) => async (dispatch, ...args) => {
   dispatch({ type: ADD_MESSAGE, payload: data });
 };
 
-export { addRoom, addMessage, sendMessage };
+export { addRoom, sendMessage, changeRoom };
