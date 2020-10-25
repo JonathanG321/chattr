f7216dc88bfcd58d2cca74abe887c4c0ac73f318
JonathanG321 <JonathanG321@ymail.com>
2020-10-25 15:30:05 -0400

    add message when sending message

--- client/src/HigherOrderComponents/RoomHOC.js
+++ client/src/HigherOrderComponents/RoomHOC.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { addRoom, sendMessage, changeRoom } from '../actions/roomActions';
+import { changeRoom, addMessage } from '../actions/roomActions';
 
 function RoomHOC(Component) {
   const mapStateToProps = (state) => ({
@@ -9,27 +9,21 @@ function RoomHOC(Component) {
     socket: state.socket.socket,
   });
   const mapDispatchToProps = {
-    addRoom,
     changeRoom,
-    sendMessage,
+    addMessage,
   };
   return connect(
     mapStateToProps,
     mapDispatchToProps,
   )((props) => {
-    function addRoom(room) {
-      return props.addRoom(room);
-    }
     function changeRoom(roomName) {
       return props.changeRoom(roomName);
     }
     function sendMessage(message) {
-      console.log(message);
       props.socket.emit('message', message);
+      return props.addMessage(message);
     }
-    return (
-      <Component {...props} changeRoom={changeRoom} addRoom={addRoom} sendMessage={sendMessage} />
-    );
+    return <Component {...props} changeRoom={changeRoom} sendMessage={sendMessage} />;
   });
 }
 
